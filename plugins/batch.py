68d1695d935830b8bfd4a4d1407a72343b3ba1dc
yogeshmirro <101546464+yogeshmirro@users.noreply.github.com>
2023-01-27 19:21:18 +0530

    Update batch.py

--- plugins/batch.py
+++ plugins/batch.py
@@ -9,22 +9,6 @@ import re
 from PIL import Image
 from configs import Config
 import time
-# @Client.on_callback_query(filters.regex('cancel'))
-# async def cancel(bot,update):
-# 	try:
-# 		await update.message.delete()
-# 	except:
-# 		return
-# @Client.on_callback_query(filters.regex('rename'))
-# async def rename(bot,update):
-# 	user_id = update.message.chat.id
-# 	date = update.message.date
-# 	await update.message.delete()
-# 	await update.message.reply_text("__Please enter the new filename...__",	
-# 	reply_to_message_id=update.message.reply_to_message.message_id,  
-# 	reply_markup=ForceReply(True))
-	
-#@Client.on_callback_query(filters.regex("doc"))
 async def doc1(bot,msg,txt,message):
     old_name = msg.document.file_name
     if remove_word:
