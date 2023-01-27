e769516dd211362ad51bfbd448ecea7f10723c8c
yogeshmirro <101546464+yogeshmirro@users.noreply.github.com>
2023-01-27 18:45:18 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -4,10 +4,11 @@ import asyncio
 from pyrogram import Client ,filters
 from helper.database import getid
 import time
-ADMIN = int(os.environ.get("ADMIN", 923943045))
+from configs import Config
+#ADMIN = int(os.environ.get("ADMIN", 923943045))
  
 
-@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["broadcast"]))
+@Client.on_message(filters.private & filters.user(Config.ADMIN) & filters.command(["broadcast"]))
 async def broadcast(bot, message):
  if (message.reply_to_message):
    ms = await message.reply_text("Geting All ids from database ...........")
