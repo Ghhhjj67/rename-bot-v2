ab2a235c5a76d69bda8cfc098c769c7554f46047
yogeshmirro <101546464+yogeshmirro@users.noreply.github.com>
2023-01-31 18:17:26 +0530

    Update filedetect.py

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -6,12 +6,12 @@ async def refunc(client,message):
         if (message.reply_to_message.reply_markup) and isinstance(message.reply_to_message.reply_markup, ForceReply):
         	new_name = message.text
         	await message.delete()
-        	media = await client.get_messages(message.chat.id,message.reply_to_message.message_id)
+        	media = await client.get_messages(message.chat.id,message.reply_to_message.id)
         	file = media.reply_to_message.document or media.reply_to_message.video or media.reply_to_message.audio
         	filename = file.file_name
         	types = file.mime_type.split("/")
         	mime = types[0]
-        	mg_id = media.reply_to_message.message_id
+        	mg_id = media.reply_to_message.id
         	try:
         		out = new_name.split(".")
         		out_name = out[1]
@@ -27,7 +27,7 @@ async def refunc(client,message):
         		else:
         			markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📁 Document",callback_data = "doc") ]])
         		# dont chenge this message.reply_text     			        		
-        		await message.reply_text(f"**Select the output file type**\n**Output FileName** :- ```{out_filename}```",reply_to_message_id=mg_id,reply_markup = markup)
+        		await message.reply_text(f"**Select the output file type**\n**Output FileName** :- {out_filename}",reply_to_message_id=mg_id,reply_markup = markup)
         		
         	except:
         		try:
@@ -49,6 +49,6 @@ async def refunc(client,message):
         		else:
         			markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📁 Document",callback_data = "doc") ]])
         		# dont chenge this message.reply_text 
-        		await message.reply_text(f"**Select the output file type**\n**Output FileName** :- ```{out_filename}```",
+        		await message.reply_text(f"**Select the output file type**\n**Output FileName** :- {out_filename}",
         		reply_to_message_id=mg_id,reply_markup = markup)
         		
